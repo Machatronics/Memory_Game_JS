1929420c4c02e09a2fbba67675292371beb910b8
Machatronics <berkaysarac93@gmail.com>
2022-03-01 23:23:17 +0100

    Next

--- app.js
+++ app.js
@@ -1,55 +1,55 @@
 const cardArray = [
     {
-      name: 'fries',
-      img: 'images/fries.png'
+        name: 'fries',
+        img: 'images/fries.png',
     },
     {
-      name: 'cheeseburger',
-      img: 'images/cheeseburger.png'
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png',
     },
     {
-      name: 'ice-cream',
-      img: 'images/ice-cream.png'
+        name: 'hotdog',
+        img: 'images/hotdog.png',
     },
     {
-      name: 'pizza',
-      img: 'images/pizza.png'
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
     },
     {
-      name: 'milkshake',
-      img: 'images/milkshake.png'
+        name:'milkshake',
+        img: 'images/milkshake.png',
     },
     {
-      name: 'hotdog',
-      img: 'images/hotdog.png'
+        name:'pizza',
+        img: 'images/pizza.png'
+    },
+        {
+        name: 'fries',
+        img: 'images/fries.png',
     },
     {
-      name: 'fries',
-      img: 'images/fries.png'
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png',
     },
     {
-      name: 'cheeseburger',
-      img: 'images/cheeseburger.png'
+        name: 'hotdog',
+        img: 'images/hotdog.png',
     },
     {
-      name: 'ice-cream',
-      img: 'images/ice-cream.png'
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
     },
     {
-      name: 'pizza',
-      img: 'images/pizza.png'
+        name:'milkshake',
+        img: 'images/milkshake.png',
     },
     {
-      name: 'milkshake',
-      img: 'images/milkshake.png'
-    },
-    {
-      name: 'hotdog',
-      img: 'images/hotdog.png'
+        name:'pizza',
+        img: 'images/pizza.png'
     }
-  ]
+]
 
-cardArray.sort(() => 0.5 - Math.random())
+console.log(cardArray)
 
 const gridDisplay = document.querySelector('#grid')  // # = id
 
